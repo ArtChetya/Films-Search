a47acd62c54772bd73655b1ed1cb9f2de1f4f8f3
Artem Chetverikov <art.chetya30@gmail.com>
2019-03-11 18:20:07 +0200

    -- added core dependencies -- added base webpack config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const plugins = [
+    new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+    })
+];
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx"]
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: "/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: './dist',
+        port: 4200,
+        historyApiFallback: true,
+        hotOnly: true
+    },
+    plugins,
+};
