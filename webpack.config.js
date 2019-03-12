e7b61598ce7504a35a3d835f40a5116c6595659a
Artem Chetverikov <art.chetya30@gmail.com>
2019-03-12 15:38:53 +0200

    -- added DEV and PROD build commands

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,76 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-const plugins = [
-    new HtmlWebPackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html"
-    })
-];
+module.exports = (env, argv) => {
+    const { mode } = argv;
+    const isProduction = mode === 'production';
 
-module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.gif$/,
-                loader: 'url-loader'
-            },
-            {
-                test: /\.(ttf|eot|svg)$/,
-                loader: 'file-loader'
-            },
-        ]
-    },
-    resolve: {
-        extensions: ["*", ".js", ".jsx"]
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: "/",
-        filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: './dist',
-        port: 4200,
-        historyApiFallback: true,
-        hotOnly: true
-    },
-    plugins,
+    let plugins = [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: isProduction ? '[name].[hash].css' : '[name].css',
+            chunkFilename: isProduction ? '[id].[hash].css' : '[id].css',
+        })
+    ];
+
+    if (isProduction) {
+        const prodPlugins = [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production')
+            }),
+            new OptimizeCSSAssetsPlugin()
+        ];
+
+        plugins = [...plugins, ...prodPlugins];
+    }
+
+    return {
+        entry: './src/index.js',
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.gif$/,
+                    loader: 'url-loader'
+                },
+                {
+                    test: /\.(ttf|eot|svg)$/,
+                    loader: 'file-loader'
+                },
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx']
+        },
+        output: {
+            path: __dirname + '/dist',
+            publicPath: '/',
+            filename: 'bundle.js'
+        },
+        devServer: {
+            contentBase: './dist',
+            port: 4200,
+            historyApiFallback: true,
+            hotOnly: true
+        },
+        devtool: isProduction ? false : 'inline-source-map',
+        mode,
+        plugins,
+    };
 };
