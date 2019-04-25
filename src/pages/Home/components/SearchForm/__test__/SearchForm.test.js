0a795940f8a3aeae8b82165bb98bca216c7a77ee
Artem Chetverikov <art.chetya30@gmail.com>
2019-04-25 16:23:03 +0300

    -- fixed warnings in tests

--- src/pages/Home/components/SearchForm/__test__/SearchForm.test.js
+++ src/pages/Home/components/SearchForm/__test__/SearchForm.test.js
@@ -3,20 +3,36 @@ import React from 'react';
 import { SearchForm } from '..';
 
 describe('SearchForm component', () => {
+    let search;
+    let searchBy;
     let onSearch;
 
     beforeAll(() => {
+        search = '';
+        searchBy = 'title';
         onSearch = jest.fn();
     });
 
     it('should be rendered correctly', () => {
-        const component = shallow(<SearchForm onSearch={onSearch} />);
+        const component = shallow(
+            <SearchForm
+                search={search}
+                searchBy={searchBy}
+                onSearch={onSearch}
+            />
+        );
         expect(component).toMatchSnapshot();
     });
 
     it('should change value of input on change', () => {
         const expectedValue = 'someValue';
-        const component = mount(<SearchForm onSearch={onSearch} />);
+        const component = mount(
+            <SearchForm
+                search={search}
+                searchBy={searchBy}
+                onSearch={onSearch}
+            />
+        );
 
         component.find('input').simulate('change', {
             target: { value: expectedValue }
@@ -26,7 +42,13 @@ describe('SearchForm component', () => {
     });
 
     it('should call onSearch of form submit', () => {
-        const component = mount(<SearchForm onSearch={onSearch} />);
+        const component = mount(
+            <SearchForm
+                search={search}
+                searchBy={searchBy}
+                onSearch={onSearch}
+            />
+        );
         component.find('form').simulate('submit');
 
         expect(onSearch).toHaveBeenCalled();
